7ec6442eab39f6db8f1de57dcb078f233beb1134
Rockwell 95 <dominick.mancini@uoit.net>
2016-03-06 21:33:00 -0500

    Removed capital letters from scott

---
+++ newest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+
+  (find . -type f -path "*Assignment\ 1*" -not -path "*actual\ output*" -not -path "*expected\ output*" ! -name 'output.txt' ! -name 'CSCI*' ! -name 'dummy.txt' ! -name 'failedtests.txt' -name '*.txt' ) | while read -r fileLine
+  do
+    PRE=$(echo $fileLine | grep -o -P '\.(.*?admin|.*?standard)')
+    POST=$(echo $fileLine | grep -o '\w*.txt')
+    FILEOUT=$(echo "$PRE""/actual output/$POST")
+    echo "Evaluating $fileLine..."
+    echo "Creating file $FILEOUT..."
+    mkdir -p $PRE"/actual output/"
+  while read -r fileContents
+    do
+      echo $fileContents | ./Phase2_unix_prototype/Phase2.exe > "$FILEOUT"
+    done < $fileLine
+  done
+
+IFS=$SAVEIFS
