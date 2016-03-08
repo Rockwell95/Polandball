f7e59ad89ae424dead8bc5f159b4063a47f00d35
Rockwell 95 <dominick.mancini@uoit.net>
2016-03-08 17:32:10 -0500

    Added while loops to main

--- Phase4_unix/src/Phase4/Main.java
+++ Phase4_unix/src/Phase4/Main.java
@@ -1,10 +1,12 @@
 package Phase4;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws FileNotFoundException {
 		File fMasterTransactionFile = new File("master_accounts.txt");
 		File fOldMasterAccountsFile = new File("current_accounts.txt");
@@ -12,6 +14,18 @@ public class Main {
 		Scanner scTransaction = new Scanner(fMasterTransactionFile);
 		Scanner scAccounts = new Scanner(fOldMasterAccountsFile);
 		
+		ArrayList<Transaction> arrayOfTransactions = new ArrayList<Transaction>();
+		ArrayList<Account> arrayOfAccounts = new ArrayList<Account>();
+		
+		Utilities u = new Utilities();
+		
+		while(scTransaction.hasNextLine()) {
+			arrayOfTransactions.add(new Transaction(scTransaction.nextLine()));
+		}
+		
+		while(scAccounts.hasNextLine()) {
+			arrayOfAccounts.add(new Account(scAccounts.nextLine()));
+		}
 	}
 
 }
