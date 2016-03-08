9357e475007688159276c820b51f3582ee5ca576
Rockwell95 <dominick.mancini@uoit.net>
2016-03-08 18:10:08 -0500

    Wip Phase 4

--- Phase4_unix/src/Phase4/Main.java
+++ Phase4_unix/src/Phase4/Main.java
@@ -1,13 +1,14 @@
 package Phase4;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	@SuppressWarnings("resource")
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, NotDirectoryException {
 		File fMasterTransactionFile = new File("master_accounts.txt");
 		File fOldMasterAccountsFile = new File("current_accounts.txt");
 		
@@ -17,10 +18,21 @@ public class Main {
 		ArrayList<Transaction> arrayOfTransactions = new ArrayList<Transaction>();
 		ArrayList<Account> arrayOfAccounts = new ArrayList<Account>();
 		
+		String transactionString;
+		
 		Utilities u = new Utilities();
 		
 		while(scTransaction.hasNextLine()) {
-			arrayOfTransactions.add(new Transaction(scTransaction.nextLine()));
+			transactionString = scTransaction.nextLine();
+			if(transactionString.length() != 40){
+				String culpritFile = u.findFile(transactionString);
+				System.err.println("ERROR: Invalid transaction, located in transaction file " + culpritFile + "."
+						+ "Transaction: " + transactionString);
+			}
+			else {
+				arrayOfTransactions.add(new Transaction(scTransaction.nextLine()));
+			}
+			
 		}
 		
 		while(scAccounts.hasNextLine()) {
