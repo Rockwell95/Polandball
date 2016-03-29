ee23016d1b8a491ff3787fa73868d98fc2caf953
Rockwell95 <dominick.mancini@uoit.net>
2016-03-28 22:14:49 -0400

    Added New Test

--- Phase4_unix/src/Phase4/TransactionTesting.java
+++ Phase4_unix/src/Phase4/TransactionTesting.java
@@ -4,105 +4,161 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.nio.file.NotDirectoryException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Scanner;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class TransactionTesting {
 	
 	//----GLOBAL VARIABLES FOR MASTER ACCOUNTS FILE-----------------------
-	private String acctLine;
 	private ArrayList<Account> arrayOfAccounts = new ArrayList<Account>();
 	//--------------------------------------------------------------------
+	 
+	@Before
+	public void setup() throws FileNotFoundException, NotDirectoryException{
+		File masterAccountFile = new File("master_accounts.txt");
+		arrayOfAccounts = Utilities.getArrayOfAccounts(masterAccountFile);	
+	}
 	
 	@Test
-	public void test() throws FileNotFoundException, NotDirectoryException{
-		
-		//-----------DELETES PREVIOUS MERGED TEST TRANSACTION FILE--------
-		Path mergedPath = Paths.get("./transaction_tests/merged_test_files/merged_master_transactions.txt");
-		Utilities.tryDelete(mergedPath);
-		//----------------------------------------------------------------
-		
-		//--------------INITIALIZE VARIABLES------------------------------
-		ArrayList<Boolean> testOutputs = new ArrayList<Boolean>();
-		
-		String transactionLine;
-		
-		//List of Accounts and Transactions
-		ArrayList<Transaction> arrayOfTransactions = new ArrayList<Transaction>();
-		
-		ArrayList<File> resultFiles = new ArrayList<File>();
-		ArrayList<File> expectedFiles = new ArrayList<File>();
-		
-		File[] trans = Utilities.getIndividualTransactionFiles("./transaction_tests/test_files/");
-		File masterTransactionFile = Utilities.mergeFiles(trans, "./transaction_tests/merged_test_files/merged_master_transactions.txt");
-		
-		Scanner transScanner = new Scanner(masterTransactionFile);
-		//----------------------------------------------------------------
-		
-		//Sets arrayOfAccounts to what is in the master accounts file before first test
-		resetAccounts();
-		
-		
-		
-		//----------READS EACH TRANSACTION--------------------------------
-		while(transScanner.hasNextLine()){
-			transactionLine = transScanner.nextLine();
-			if(transactionLine.length() == 41){
-				arrayOfTransactions.add(new Transaction(transactionLine));
-			}
-		}
-		transScanner.close();
-		//----------------------------------------------------------------
-		
-		//-------------------RUNS EACH TRANSACTION-----------------------
-		int j = 0;
-		while(j < arrayOfTransactions.size()){
-			Transaction currentTransaction = arrayOfTransactions.get(j);
-			File result = new File("./transaction_tests/actual result files/TransactionResult" + (j+1) + ".txt");
-	    	File eResult = new File("./transaction_tests/expected result files/TransactionResult" + (j+1) + ".txt");
-	    	FileOutputStream rof = new FileOutputStream(result);
-	    	PrintStream resultPrint = new PrintStream(rof);
-
-	    	System.setErr(resultPrint);
-	    	testOutputs.add(Utilities.processTransaction(currentTransaction, arrayOfAccounts));
-	    	
-	    	resultFiles.add(result);
-	    	expectedFiles.add(eResult);
-	    	
-	    	
-	    	resetAccounts();
-			j++;
-			
-		}
-		//----------------------------------------------------------------
-		
-		//----------TESTS EACH TRANSACTION AGAINST EXPECTED OUTPUT--------
-		for(int i=0; i<testOutputs.size(); i++){
-			assertEquals("FAILED TEST", new Scanner(resultFiles.get(i)).useDelimiter("\\Z").next(), new Scanner(expectedFiles.get(i)).useDelimiter("\\Z").next());
-		}
-		//----------------------------------------------------------------
-		
-	}	
-	//------------------RESETS THE ACCOUNTS ARRAY TO ORIGINAL-------------
-	public void resetAccounts() throws FileNotFoundException{
-		File masterAccountFile = new File("./transaction_tests/initial_master_accounts.txt");
-		Scanner readAccounts = new Scanner(masterAccountFile);
-	
-		while(readAccounts.hasNextLine()){
-			acctLine = readAccounts.nextLine();
-		
-			if(acctLine.length() == 44){
-				arrayOfAccounts.add(new Account(acctLine));
-			}
-		}
-		readAccounts.close();
+	public void createAccountTestSuccess(){
+		Transaction transaction = new Transaction("05 vector prime         12351 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
 	}
-	//--------------------------------------------------------------------
+	
+	@Test
+	public void createAccountTestFail(){
+		Transaction transaction = new Transaction("05 dominick mancini     12345 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void depositFundsFail(){
+		Transaction transaction = new Transaction("04 janahan nirmalan     12350 00000.09 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void depositFundsSuccess(){
+		Transaction transaction = new Transaction("04 janahan nirmalan     12350 00300.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void depositFundsDisabled(){
+		Transaction transaction = new Transaction("04 scott mclean         12346 00010.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void withdrawFundsDisabled(){
+		Transaction transaction = new Transaction("01 scott mclean         12346 00010.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void withdrawFundsSuccess(){
+		Transaction transaction = new Transaction("01 dominick mancini     12345 00010.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void withdrawFundsFailure(){
+		Transaction transaction = new Transaction("01 dominick mancini     12345 00200.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts),false);
+	}
+	
+	@Test
+	public void paybillCompanyDoesNotExist(){
+		Transaction transaction = new Transaction("03 dominick mancini     12345 00050.00 TT");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts),false);
+	}
+	
+	@Test
+	public void paybillSuccess(){
+		Transaction transaction = new Transaction("03 dominick mancini     12345 00050.00 TV");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void paybillInsufficientFunds(){
+		Transaction transaction = new Transaction("03 dominick mancini     12345 00200.00 TV");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void paybillDisabled(){
+		Transaction transaction = new Transaction("03 scott mclean         12346 00050.00 TV");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void withdrawNonExistantAccount(){
+		Transaction transaction = new Transaction("01 bob mclean           15736 00050.00 MM");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void depositNonExistantAccount(){
+		Transaction transaction = new Transaction("04 bob mclean           15736 00050.00 MM");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void paybillNonExistantAccount(){
+		Transaction transaction = new Transaction("01 bob mclean           15736 00050.00 TV");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}	
+	
+	@Test
+	public void deleteAccountSuccess(){
+		Transaction transaction = new Transaction("06 dominick mancini     12345 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void deleteAccountFailure(){
+		Transaction transaction = new Transaction("06 bob mclean           15736 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void changeplanSuccess(){
+		Transaction transaction = new Transaction("08 dominick mancini     12345 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void changeplanFailure(){
+		Transaction transaction = new Transaction("08 bob mclean           15736 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction,arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void disableAccountSucess(){
+		Transaction transaction = new Transaction("07 dominick mancini     12345 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void disableAccountFailure(){
+		Transaction transaction = new Transaction("07 scott mclean         12346 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	@Test
+	public void enableAccountSucess(){
+		Transaction transaction = new Transaction("09 scott mclean         12346 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), true);
+	}
+	
+	@Test
+	public void enableAccountFailure(){
+		Transaction transaction = new Transaction("09 dominick mancini     12345 00000.00 MM");
+		assertEquals(Utilities.processTransaction(transaction, arrayOfAccounts), false);
+	}
+	
+	
+	
 }
